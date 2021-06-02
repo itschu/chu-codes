6b0f34b9879ebb3b6087dcf800aed7af1efef35a
Joseph Chu <joseoh-chu@pop-os.localdomain>
2021-06-02 16:52:48 +0100

    project initialization and first commit. Installed a start up dependencies

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+ReactDOM.render(<h1>Welcome</h1>, document.getElementById('app'));
